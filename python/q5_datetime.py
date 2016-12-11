9f741b975badcdc2adac471c653114747a64cc95
Brian Maimone <bmaimone72@gmail.com>
2016-12-11 14:29:33 -0500

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -2,11 +2,24 @@
 
 ####a) 
 date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+date_stop = '07-28-2015'  
+
+from datetime import datetime
+import time
+
+print datetime.today()
+
+stop = datetime(2015, 07, 28, 0, 0)
+start =  datetime(2013, 01, 2, 0, 0)
+
+print stop - start
 
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
+
+
+
 
 ####c)  
 date_start = '15-Jan-1994'  
