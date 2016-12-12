dfe7a93e4bed7f1f82a44c25e7d83d9357a8fff3
Brian Maimone <bmaimone72@gmail.com>
2016-12-12 12:25:04 -0500

    answers q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -20,7 +20,33 @@ print stop - start
 date_start = '12312013'  
 date_stop = '05282015'  
 
+from datetime import datetime
+import time
+s = "05282015"
+s1 = '12312013'
+
+stop2 = datetime(year=int(s[4:8]), month=int(s[0:2]), day=int(s[2:4]))
+print stop2
+
+start2 = datetime(year=int(s1[4:8]), month=int(s1[0:2]), day=int(s1[2:4]))
+
+print stop2 - start2
+
+513 days
 
 ####c)  
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+
+date_start = '15-Jan-1994'
+date_stop = '14-Jul-2015'
+
+stop3 = datetime.strptime(date_stop,'%d-%b-%Y').date()
+
+start3 = datetime.strptime(date_start,'%d-%b-%Y').date()
+
+print stop3 - start3
+
+7850 days
+
+
