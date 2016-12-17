be2e259328f0cc3c8f57c9785e4ec06b7d707a57
Brian Maimone <bmaimone72@gmail.com>
2016-12-17 11:14:20 -0500

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,16 @@
-PLACE YOUR CODE HERE
+from csv import writer
+
+from advanced_python_regex import read_data, get_emails
+
+data = read_data('faculty.csv')
+
+emails = [x[0] for x in get_emails(data)]
+
+
+with open('emails.csv', 'wb') as csvfile:
+    
+    email_writer = writer(csvfile)
+    
+    for email in emails:
+        
+        email_writer.writerow([email])
