7503ca374d502db8a685876d77918e2a17a7f89d
Brian Maimone <bmaimone72@gmail.com>
2016-12-17 11:10:35 -0500

    Update q7_lists.py

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -86,29 +86,41 @@ def linear_merge(list1, list2):
 import re
 
 def read_data(file_name):
+    
     with open(file_name, 'rb') as inputfile:
+        
         data = inputfile.read()
+        
     return data
 
 
 def find_unique(regex, data):
+    
     results = [x[0] for x in re.findall(regex, data)]
+    
     return {x : results.count(x) for x in results}
 
 
 def print_unique(name, d):
+    
     print 'There are ', len(d),' unique ', name, '.'
+    
     print 'Frequencies are:'
+    
     for k, f in d.iteritems():
+        
         print k, ':', f
 
 
 def print_sequence(s):
+    
     for x in s:
+        
         print x
 
 
 def get_emails(data):
+    
     return re.findall(r'([\w.-]+@([\w.-]+))', data)
 
 
@@ -127,18 +139,24 @@ def main():
     # Q3
 
     emails = get_emails(data)
+    
     print 'Emails:'
+    
     print_sequence([x[0] for x in emails])
 
     # Q4
 
     unique_domains = set([x[1] for x in emails])
+    
     print 'There are ', len(unique_domains), ' unique email domains.'
+    
     print 'They are:'
+    
     print_sequence(unique_domains)
 
 
 if __name__ == "__main__":
+    
     main()
 
     
